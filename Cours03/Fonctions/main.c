ab21a5de1fdfcc73be2cd56ea37fd06a96210be7
Anis Boubaker <anis@boubaker.ca>
2023-09-29 10:31:20 -0400

    Cours 03

--- Cours03/Fonctions/main.c
+++ Cours03/Fonctions/main.c
@@ -31,21 +31,49 @@ double cube(double valeur)
     return valeur * valeur * valeur;
 }
 
+// void comme paramètre veut dire que la fonction n'a pas besoin de paramètre
+// pour effectuer son traitement.
+int saisir_entier_positif( void )
+{
+    int saisie;
+
+    do
+    {
+        printf("Saisir une valeur positive: ");
+        scanf("%i", &saisie);
+    } while (saisie <=0 );
+
+    return saisie;
+}
+
+void afficher_message_bienvenue(void)
+{
+    printf("*********************************************\n");
+    printf("Bienvenue dans mon super programme!!\n");
+    printf("*********************************************\n");
+
+    //Le return est non-nécessaire car la fonction ne retourne rien.
+    //return ;
+}
+
+
 int main(void)
 {
     int resultat;
     int min, max;
     double calcul_cube;
 
-    printf("Saisir la valeur du debut: ");
-    scanf("%i", &min);
+    afficher_message_bienvenue();
 
-    printf("Saisir la valeur de la fin: ");
-    scanf("%i", &max);
+    //Remarquez les parenthèse vides: elles sont nécessaires même si la fonction
+    //ne prend pas de paramètres!
+    min = saisir_entier_positif() ;
+
+    max = saisir_entier_positif();
 
     resultat = somme_des_pairs(min, max);
 
-    printf("Le resultat est: %i\n", resultat);
+    printf("La somme des valeurs entre %i et %i: %i\n", min, max, resultat);
 
     //calcul_cube = cube(10);
     printf("10 au cube = %lf\n", cube(10) );
